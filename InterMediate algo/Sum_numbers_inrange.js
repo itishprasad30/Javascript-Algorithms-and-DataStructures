e36f509eb7ea425c46ac1b8491ef43178f2e63b0
itishprasad30 <itishprasad20@gmail.com>
2021-12-11 21:05:10 +0530

    Intemidiate algorithm added

---
+++ InterMediate algo/Sum_numbers_inrange.js
@@ -0,0 +1,33 @@
+function sumAll(arr) {
+  let [min, max] = [...arr].sort((a, b) => a - b);
+  console.log("Min", min);
+  console.log("Max", max);
+  let sumInitial = 0;
+  for (let i = min; i <= max; i++) {
+    sumInitial += i;
+  }
+  return sumInitial;
+}
+
+console.log(sumAll([100, 1]));
+
+// Find the different from two array
+
+const differArr = (arr1, arr2) => {
+  let newArr = [];
+
+  let onlyFirst = (first, second) => {
+    for (let i = 0; i < first.length; i++) {
+      if (second.indexOf(first[i]) === -1) {
+        newArr.push(first[i]);
+      }
+    }
+  };
+
+  onlyFirst(arr1, arr2);
+  onlyFirst(arr2, arr1);
+
+  return newArr;
+};
+
+console.log(differArr([1, 2, 3], [2, 3, 4, 5]));
