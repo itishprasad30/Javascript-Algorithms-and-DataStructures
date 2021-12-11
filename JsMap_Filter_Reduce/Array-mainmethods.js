672009bf10da6ce6cc095d9e9647e099ee835374
itishprasad30 <itishprasad20@gmail.com>
2021-12-11 16:03:03 +0530

    Array methods map is added

---
+++ JsMap_Filter_Reduce/Array-mainmethods.js
@@ -0,0 +1,7 @@
+let numbers = [3, 4, 43, 12, 5, 313, 554];
+
+//Map -Creates a new array by doing someting with each item in an array
+
+const newNum = numbers.map((num) => {
+  console.log(num);
+});
