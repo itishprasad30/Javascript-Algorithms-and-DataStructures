532e87e467011ec8b03cd022bc3b98f0a389c12b
itishprasad30 <itishprasad20@gmail.com>
2021-12-11 16:22:20 +0530

    filter method added

--- JsMap_Filter_Reduce/Array-mainmethods.js
+++ JsMap_Filter_Reduce/Array-mainmethods.js
@@ -1,7 +1,18 @@
-let numbers = [3, 4, 43, 12, 5, 313, 554];
+let numbers = [1, 2, 3, 4, 5];
 
 //Map -Creates a new array by doing someting with each item in an array
 
-const newNum = numbers.map((num) => {
-  console.log(num);
+const mapingItem = numbers.map((num) => {
+  return num;
 });
+
+console.log(mapingItem); //returns:➡️ [1,2,3,4,5]
+
+// Filter -Creates a new array by keeping the items that return true
+
+var rates = [10, 20, 30, 40, 50];
+const filterItems = rates.filter((num) => {
+  return num >= 40;
+});
+
+console.log(filterItems); //returns:➡️ [40,50]
