7c6a02f8a7ecc1fd2a304653aff930fe0ed6575b
itishprasad30 <itishprasad20@gmail.com>
2021-12-13 07:49:51 +0530

    nos 3 added

---
+++ SomeBasic Js/3.Reverse-Int.js
@@ -0,0 +1,13 @@
+const num = 3849;
+
+function reverseGivenInteger(num) {
+  // write your solution here
+  let reversedNum = 0;
+  while (num > 0) {
+    reversedNum = reversedNum * 10 + (num % 10);
+    num = Math.floor(num / 10);
+  }
+  return reversedNum;
+}
+
+console.log(`Reversed integer is: ${reverseGivenInteger(num)}`);
