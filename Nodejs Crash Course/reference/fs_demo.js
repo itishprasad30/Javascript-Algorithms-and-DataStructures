04300d22c6878c7ff9c5642443197a7ebef04522
itishprasad30 <itishprasad20@gmail.com>
2021-12-22 11:16:25 +0530

    fs understanding concepts

--- Nodejs Crash Course/reference/fs_demo.js
+++ Nodejs Crash Course/reference/fs_demo.js
@@ -9,20 +9,37 @@ const path = require("path");
 
 // make a file and write in this
 
-fs.writeFile(
-  path.join(__dirname, "/test", "demo.txt"),
-  "HEllo node js",
+// fs.writeFile(
+//   path.join(__dirname, "/test", "demo.txt"),
+//   "HEllo node js",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("File is created ...");
+//     //callback for appending a new text in this same file
+//     fs.appendFile(
+//       path.join(__dirname, "/test", "demo.txt"),
+//       " I love js",
+//       (err) => {
+//         if (err) throw err;
+//         console.log("File is apended ...");
+//       }
+//     );
+//   }
+// );
+
+// reading the file
+// fs.readFile(path.join(__dirname, "/test", "demo.txt"), "utf-8", (err, data) => {
+//   if (err) throw err;
+//   console.log(data);
+// });
+
+// rename the file from existing file
+
+fs.rename(
+  path.join(__dirname, "/text", "demo.txt"),
+  path.join(__dirname, "/text", "demo1.txt"),
   (err) => {
     if (err) throw err;
-    console.log("File is created ...");
-    //callback for appending a new text in this same file
-    fs.appendFile(
-      path.join(__dirname, "/test", "demo.txt"),
-      " I love js",
-      (err) => {
-        if (err) throw err;
-        console.log("File is apended ...");
-      }
-    );
+    console.log("THe file is renamed");
   }
 );
