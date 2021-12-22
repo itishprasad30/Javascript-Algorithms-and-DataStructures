ee65524efb77747716545802c690f83a986e9672
itishprasad30 <itishprasad20@gmail.com>
2021-12-22 12:05:31 +0530

    os info by node js

---
+++ Nodejs Crash Course/reference/os_demo.js
@@ -0,0 +1,21 @@
+const os = require("os");
+
+// Os platform
+console.log(os.platform());
+
+// os arch()
+console.log(os.arch());
+// OS  cpu info
+console.log(os.cpus());
+
+// free memory
+console.log(os.freemem());
+
+// total memory
+console.log(os.totalmem());
+
+//home dir
+console.log(os.homedir());
+
+// uptime
+console.log(os.uptime());
